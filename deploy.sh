b519f404a52fbeb22be1416c5f557fbc9fee0bfe
terry.chen <zxtw17985321@gmail.com>
2021-03-14 16:03:12 +0800

    feat: deploy

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd src/.vuepress/dist
+
+# git step
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:terry90918/terry90918.github.io.git master:gh-pages
+
+
+cd -
