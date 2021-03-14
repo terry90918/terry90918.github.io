f91286a8a91f4d75a7240ee000ebeccc7662aa8f
terry.chen <zxtw17985321@gmail.com>
2021-03-14 15:39:00 +0800

    first commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,74 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Terry Blog',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: '部落格',
+        link: '/blog/',
+      },
+      {
+        text: '知識庫',
+        link: '/wiki/'
+      },
+      {
+        text: '作品集',
+        link: '/side-project/'
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/terry90918/blog'
+      }
+    ],
+    sidebar: {
+      '/blog/': [
+        {
+          title: 'Blog',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: https://v1.vuepress.vuejs.org/hero.png
+heroText: Blog
+tagline: 歡迎來到部落格
+actionText: Quick Start →
+actionLink: /guide/
+features:
+- title: 部落格
+  details: 生活文章、技術文章
+- title: 知識庫
+  details: 知識脈絡梳理
+- title: 作品集
+  details: 個人作品展示區
+footer: Made by terry90918 with ❤️
+---
